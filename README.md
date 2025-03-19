89919f4d6c4652bd165f98675aec3f0c3fe54e90
Sebastian Gonzalez <seba.gnh@gmail.com>
2025-03-19 01:45:08 -0300

    Se agregan validaciones notAdmin y notBlacklisted en el correo

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
-# React + TypeScript + Vite
+# React + TypeScript + Vite         ```javascript ```
 touch README.md <-- creamos el archivo
+
+
+# Instalar React Hook Form
+yarn add react-hook-form
+
+# Instalar Devtools
+yarn add -D @hookform/devtools
+
+
+
+ ```javascript
+const { name, ref, onChange, onBlur } = register('username');
+
+<label htmlFor='username' className="prueba">Username</label>
+<input
+  type='text'
+  autoComplete="off"
+  id='username'
+  name={name}
+  ref={ref}
+  onChange={onChange}
+  onBlur={onBlur}
+/>
+
+
+ // es lo mismo que arriba, pero simplificado
+// {...register('username')}  devuelve un objeto con name, ref, onChange, y onBlur.
+<input
+  type='text'
+  autoComplete="off"
+  id='username'
+  {...register('username')}
+/>
+/>
+```
+
+
+
+
+
+
+
+
+/*   width: 100vw;
+  height: 100vh;
+  position: relative;
+  max-width: 1920px;
+  max-height: 1080px;
+  aspect-ratio: 16/9;
+  margin-block: 0;
+  margin-inline: auto;
+  overflow: hidden; */
 
--- src/components/Form01/Form01.tsx
+++ src/components/Form01/Form01.tsx
@@ -1,25 +1,86 @@
-import { ButtonBack } from "../../shared/ButtonBack"
+import { ButtonBack } from "../../shared/ButtonBack";
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
 
+let rerenders = 1;
+
+interface FormI {
+  username: string;
+  email: string;
+  channel: string;
+}
 
 export const Form01 = () => {
+  const {
+    register, // Función para registrar los campos del formulario
+    control, // Objeto de control para DevTools
+    handleSubmit, // Función para manejar el envío del formulario
+    formState: { errors }, // Objeto que contiene los errores de validación
+  } = useForm<FormI>();
+
+  const onSubmit = (data: FormI) => {
+    console.log('Submited', data);
+  };
+
   return (
-    <div className="container d-flex flex-column  mt-5">
-      <ButtonBack/>
-      <div className="d-flex justify-content-center">
-        <form className="d-flex align-items-start flex-column gap-2">
-          <label htmlFor='username'>Username</label>
-          <input type='text' id='username' name='username'></input>
+    <div className="container d-flex flex-column mt-5">
+      <ButtonBack />
+
+      <div className="d-flex justify-content-center contenedor1">
+        <form
+          className="d-flex align-items-start flex-column gap-2 contenedor2"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate // Desactiva la validación nativa del navegador
+        >
+          <h1>Re Renderizaciones ({rerenders})</h1>
+
+          <label htmlFor='username' className="prueba">Username</label>
+          <input
+            type='text'
+            autoComplete="off"
+            id='username'
+            {...register('username', { required: 'Usuario Requerido' })} // Registra el campo y lo hace obligatorio
+          />
+          <p className="text-danger">{errors.username?.message}</p>
 
           <label htmlFor='email'>E-Mail</label>
-          <input type='text' id='email' name='email'></input>
+          <input
+            type='text'
+            id='email'
+            autoComplete="off"
+            {...register('email', {
+              required: 'Email Requerido', // Validación requerida
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, // Validación de formato de email
+                message: 'E-mail Invalido',
+              },
+              validate: {
+                // Validación personalizada 1: Evitar un email específico
+                notAdmin: (fieldValue) =>
+                  fieldValue !== 'admin@example.com' || 'Ingresar un email diferente',
+                // Validación personalizada 2: Evitar dominios bloqueados
+                notBlackListed: (fieldValue) =>
+                  !fieldValue.endsWith('hotmail.com') || 'Dominio no soportado',
+              },
+            })}
+          />
+          <p className="text-danger">{errors.email?.message}</p>
+
 
           <label htmlFor='channel'>Channel</label>
-          <input type='text' id='channel' name='channel'></input>
-
+          <input
+            type='text'
+            id='channel'
+            autoComplete="off"
+            {...register('channel', { required: 'Canal Requerido' })} // Registra el campo y lo hace obligatorio
+          />
+          <p className="text-danger">{errors.channel?.message}</p>
 
           <button type="submit" className="btn btn-primary my-2">Submit</button>
         </form>
+
+        <DevTool control={control} />
       </div>
     </div>
-  )
-}
+  );
+};
